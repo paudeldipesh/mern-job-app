626c0c1efb471c7d617c47e523c6111f72799e19
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2023-12-21 18:21:32 +0545

    setup error page

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,12 +1,26 @@
 import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import notFound from "../assets/images/notFound.svg";
 
 export default function Error() {
   const error = useRouteError();
 
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={notFound} alt="not found icon" />
+          <h3>Page Not Found</h3>
+          <p>The page you are looking for is not found.</p>
+          <Link to="/dashboard">Back Home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
   return (
-    <div>
-      <h1>{error.statusText}</h1>
-      <Link to="/">Back Home</Link>
-    </div>
+    <Wrapper>
+      <h3>Something Went Wrong</h3>
+    </Wrapper>
   );
 }
