b3cbfb64154cbbf79f2f424e5344b855862ebb07
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2023-12-23 16:34:02 +0545

    setup register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,38 @@
 import { Link } from "react-router-dom";
+import Logo from "../components/Logo";
+import FormRow from "../components/FormRow";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 
 export default function Register() {
   return (
-    <div>
-      <h2>Register Page</h2>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" defaultValue="Dipesh" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          defaultValue="Paudel"
+        />
+        <FormRow type="text" name="location" defaultValue="Nepal" />
+        <FormRow
+          type="email"
+          name="email"
+          defaultValue="dipeshpaudel@gmail.com"
+        />
+        <FormRow type="password" name="password" defaultValue="12345678" />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 }
