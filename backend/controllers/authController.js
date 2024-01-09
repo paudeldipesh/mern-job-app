d5e90bc278ec221d721a15e420d9acd0c815b0cd
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2024-01-09 20:45:58 +0545

    setup hash password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,16 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
+import { hashedPassword } from "../utils/passwordUtils.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
+
+  const getHashedPassword = await hashedPassword(req.body.password);
+  req.body.password = getHashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 export const login = async (req, res) => {
